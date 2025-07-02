49f16363a0b7a70104389c38d4ce15596c77a64e
ashikmia0099 <ashikmia5686@gmail.com>
2025-07-02 19:46:43 +0600

    versel deploy error solve

--- src/app/(shared_tsx)/No_data.tsx
+++ src/app/(shared_tsx)/No_data.tsx
@@ -2,13 +2,14 @@
 
 import React from 'react';
 import Lottie from 'lottie-react';
+import oopsAnimation from '@/assets/animations/oops.json'; // ✅ correct ES import
 
 export default function No_data() {
   return (
     <div className="bg-white pt-[30px]">
       <div className="max-w-7xl mx-auto lg:px-[88px] px-2.5 min-h-screen flex items-center justify-center">
         <Lottie 
-          animationData={require('../../../public/json_file/oops.json')} 
+          animationData={oopsAnimation} 
           style={{ height: 400, width: 400 }}
           loop
           autoplay
